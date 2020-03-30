605e39a7f52805c1ff004df645cd74fc84255753
ericbanker12 <ericbanker12@gmail.com>
2020-03-30 17:33:07 -0500

    add column sorting, adjust styling

--- src/data/skills.js
+++ src/data/skills.js
@@ -3,7 +3,17 @@ const data = [
     technology: 'Javascript',
     category: 'Web Development',
     proficiency: 3,
-  }
+  },
+  {
+    technology: 'HTML',
+    category: 'Web Development (Front-End)',
+    proficiency: 3,
+  },
+  {
+    technology: 'CSS',
+    category: 'Web Development (Front-End)',
+    proficiency: 3,
+  },
 ]
 
 export default data

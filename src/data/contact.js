b34a9ba4f28ad7fea728c506ce60871f4084cc54
ericbanker12 <ericbanker12@gmail.com>
2020-04-02 17:11:58 -0500

    delete contact page, move contact info to footer

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,7 +1,8 @@
 const data = {
-  GitHub: 'https://github.com/EricBanker12',
-  LinkedIn: 'https://www.linkedin.com/in/eric-banker/',
-  Email: 'mailto:EricBanker12@gmail.com',
+  phone: '1-520-222-6537',
+  email: 'EricBanker12@gmail.com',
+  github: 'https://github.com/EricBanker12',
+  linkedin: 'https://www.linkedin.com/in/eric-banker/',
 }
 
 export default data

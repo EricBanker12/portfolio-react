81cba5bb185a1afc4f2606ef4825a9e5a263fcfc
ericbanker12 <ericbanker12@gmail.com>
2020-04-09 15:39:27 -0500

    Updates

--- src/data/contact.js
+++
@@ -1,8 +0,0 @@
-const data = {
-  phone: '1-520-222-6537',
-  email: 'EricBanker12@gmail.com',
-  github: 'https://github.com/EricBanker12',
-  linkedin: 'https://www.linkedin.com/in/eric-banker/',
-}
-
-export default data

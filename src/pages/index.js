7fd00774decf5b08d1dbe659f9ca99271ee067c0
ericbanker12 <ericbanker12@gmail.com>
2020-04-14 16:43:02 -0500

    edit about me

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,26 +30,26 @@ class SiteIndex extends React.Component {
         </header>
 
         <p>
-          Initially, I majored in biology to pursue a career
-          in research. However, I am now working in web and software development.
+          Initially, I earned a degree in biology to pursue a career in medical
+          research. However, I am now working in web and software development.
         </p>
         <p>
-          Prior to 2018, most of my programming experience came from my hobby
-          of creating tools and mods for video games. Towards the end of my
-          enrollment at Southern Illinois University Edwardsville,
-          I started focusing more on programming for data science in R and python,
-          and supplementing my studies with coursera.org online courses.
+          Prior to 2018, most of my programming experience came from creating
+          utilities and modifications for video games. Nearing my graduation from
+          Southern Illinois University Edwardsville, I shifted my focus toward
+          data science in R and python and supplemented my studies with extra
+          online coursework.
         </p>
         <p>
-          But after losing my brother to cancer and feeling frustrations with slow
-          progression of research, I decided to pursue a career in the fast paced
-          tech industry. This led me to attending Lambda School training in
-          full-stack web development.
+          But after losing my brother to cancer and feeling frustrations with the slow
+          progression of research to available treatments, I decided to pursue a career
+          in the fast-paced tech industry. This led me to attending Lambda School training
+          in full-stack web development.
         </p>
         <p>
           Today I boast a wide range of technical skills such as my strong background
           in HTML, CSS, JavaScript, React, Node, Express, and Jest. I have additional
-          background in R, Python, Java, C, and more from university and self-study.
+          background in R, Python, Java, C/C++, and more from university and self-study.
           I want to use my array of technical skills and biology background to improve
           people's lives in a fast-paced environment where I can see my efforts take shape.
         </p>
